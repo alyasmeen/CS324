889b5605dee27b04d4fecf5c0e00525d667c4aed
Alyasmeen <apple@MacBook-Pro-alkhas-b-apple.local>
2021-04-20 03:58:53 +0300

    Maximum matching

--- Phase3/src/phase3/Phase3.java
+++ Phase3/src/phase3/Phase3.java
@@ -9,6 +9,8 @@ class Phase3 {
     static int residual[][] = new int[V][V]; //store residual graph
 
 
+    
+    
     public static void main(String[] args){
         //Graph of the maximum flow problem
         int graph[][]={{ 0, 2, 7, 0, 0, 0 },
@@ -22,6 +24,24 @@ class Phase3 {
         System.out.println("\nMaximum flow: " + fordFulkerson(graph, 0, 5));
         System.out.println("\nMin cut:");
         minCut(graph, 0);
+        System.out.println("\n------------------------------------------------\n\n");
+        
+        
+        int bipartite[][]={{ 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+                           { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                           { 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0},
+                           { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+                           { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0},
+                           { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                           { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                           { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                           { 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                           { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                           { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}};
+        
+        System.out.println("Maximum matching:\n");
+        System.out.println("\nMaximum matching= "+maximumMatching(bipartite));
     }
     
     
@@ -149,5 +169,83 @@ class Phase3 {
                     dfs(i, visited);
                 }
         }
+    }
+    
+    //finds the maximum matching and print it, it returns the number of matchings
+    static int maximumMatching(int [][] graph){
+        int len=graph.length;
+        int n=len/2; //to iterate over the set V
+        
+        int matching []=new int[len]; //matching[u]=v means vertex u is matched with v
+        int label []=new int[len];
+        for (int i=0; i< len; i++){
+            matching[i]=-1;
+            label[i]=-1;
+        }
+        
+        LinkedList<Integer> queue= new LinkedList<>();
+        for (int i=0; i< n; i++)
+            queue.add(i);
+        
+        while(!queue.isEmpty()){
+            
+            int w=queue.poll();
+            if (w<n){ // w is in V
+                
+                for (int u=n ; u<len ; u++){
+                    
+                    if (graph[w][u]==0) //u is not adjacent to w
+                        continue;
+                    
+                    if (matching[u]==-1){ // u is free
+                        matching[w]=u; matching[u]=w;  //add edge w-u to the matching set
+                        
+                        int v=w;
+                        while (label[v] != -1){ //while v is labeled
+                            u= label[v]; matching[v]=-1; matching[u]=-1;
+                            v= label[u]; matching[v]=u; matching[u]=v;
+                        }
+                        
+                        for (int i=0; i< label.length; i++) //remove all vertex labels
+                            label[i]=-1;
+                        
+                        queue.clear();  //reinitialize queue with all free vertices in V
+                        for (int i=0; i<n; i++)
+                            if (matching[i] == -1)
+                                queue.add(i);
+                        break;
+                    }
+                    
+                    else{  //u is matched
+                        if (matching[w]!=u && label[u]==-1){
+                            label[u]=w;
+                            queue.add(u);
+                        }
+                    }
+                }
+            }
+            else{  //w is in U
+                label[matching[w]]=w;
+                queue.add(matching[w]);
+            }
+        }
+        
+        String name []= {"Ahmed", "Mahmoud", "Eman", "Fatimah", "Kamel", "Nojood",
+                        "King Abdulaziz University", "King Fahad", "East Jeddah",
+                        "King Fahad Armed Forces", "King Faisal Specialist",
+                        "Ministry of National Guard"};
+        
+        //print the matching
+        for (int i=0; i<n; i++)
+            if (matching[i]!=-1)
+                System.out.println(name[i]+"-----"+name[matching[i]]);
+
+        //count and return number of matchings
+        int maximumMatching=0;
+        for (int i=0; i<n; i++)
+            if (matching[i]!=-1)
+                maximumMatching++;
+        
+        return maximumMatching;
     }
 }
